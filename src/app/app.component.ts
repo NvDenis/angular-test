d05e22763d57d3d5e61da23a840056e2ad01bec7
nvduy248 <vduyit@gmail.com>
2023-12-06 17:34:24 +0700

    complete change color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,117 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { CdkDrag, CdkDragEnd } from '@angular/cdk/drag-drop';
+import { v4 as uuidv4 } from 'uuid';
+
+interface IButton {
+  id: string,
+  type: string,
+  text: string,
+  color: string,
+  position: { x: number, y: number },
+  visible: boolean,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'button-project';
+  isCollapsed = false;
+  savedButtons = localStorage.getItem('buttons');
+  buttons: IButton[] = this.savedButtons ? JSON.parse(this.savedButtons) : [];
+  selectedButtonText: string = ''; // Biến để lưu giữ nội dung của button được chọn
+  selectedButton: IButton | undefined
+  selectedButtonColor: string = ''
+
+  ngOnInit(): void {
+
+  }
+
+  constructor() { }
+
+  selectButton(btn: IButton): void {
+    this.selectedButton = btn;
+    this.selectedButtonText = btn.text;
+    this.selectedButtonColor = btn.color;
+  }
+
+  handleChangeColor(color: string ): void {
+    if (this.selectedButton) {
+      this.selectedButton.color = color;
+      localStorage.setItem('buttons', JSON.stringify(this.buttons));
+    }
+  }
+
+  handleAddButton(buttonData: {
+    type: string,
+    text: string,
+    color: string,
+    position: { x: number, y: number },
+    visible: boolean;
+  }): void {
+
+    const newButton: IButton = {
+      id: uuidv4(),
+      type: buttonData.type,
+      text: buttonData.text,
+      color: buttonData.color,
+      position: { x: this.buttons.length * 10, y: this.buttons.length * 10 },
+      visible: false,
+    };
+
+    this.buttons.push(newButton);
+    // lưu các nút vào localstorage
+    localStorage.setItem('buttons', JSON.stringify(this.buttons));
+  }
+
+  handleDeleteButton(id: string) {
+    const updatedButtons = this.buttons.filter(btn => btn.id !== id)
+
+    this.buttons = updatedButtons;
+    localStorage.setItem('buttons', JSON.stringify(updatedButtons));
+
+  }
+
+  updateSelectedButton(): void {
+    if (this.selectedButton) {
+      this.selectedButton.text = this.selectedButtonText;
+      localStorage.setItem('buttons', JSON.stringify(this.buttons));
+    }
+  }
+
+  // Sử dụng trackBy để chỉ định cách xác định mỗi phần tử trong danh sách
+  trackByButtonId(index: number, button: IButton): string {
+    return button.id;
+  }
+
+
+  change(value: boolean): void {
+    console.log(value);
+  }
+
+  dragEnded(event: CdkDragEnd, button: IButton): void {
+    const draggable: CdkDrag = event.source;
+    // Lấy vị trí mới của nút sau khi kéo thả
+    const newPosition = { x: draggable.getFreeDragPosition().x, y: draggable.getFreeDragPosition().y };
+
+    // Cập nhật vị trí của nút trong mảng
+    const updatedButton = { ...button, position: newPosition };
+    const updatedButtons = this.buttons.map(btn => (btn.id === updatedButton.id ? updatedButton : btn));
+
+    this.buttons = updatedButtons
+    // Lưu mảng cập nhật vào local storage
+    localStorage.setItem('buttons', JSON.stringify(updatedButtons));
+
+  }
+
+
+
+
+
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { MAT_COLOR_FORMATS, NgxMatColorPickerModule, NGX_MAT_COLOR_FORMATS } from '@angular-material-components/color-picker';
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    DragDropModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzButtonModule,
+    NzPopoverModule,
+    NzInputModule,
+    NgxMatColorPickerModule,
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
